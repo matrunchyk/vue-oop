7df9bdd3f32d8a4e3a1e0632806eb3905e1a88cc
Serhii Matrunchyk <serhii@digitalidea.studio>
2019-09-30 14:23:34 +0300

    Remove dist

--- dist/models/Exceptions/InvalidArgumentException.d.ts
+++
@@ -1,5 +0,0 @@
-import BaseException from './BaseException';
-export default class InvalidArgumentException extends BaseException {
-    message: string;
-}
-//# sourceMappingURL=InvalidArgumentException.d.ts.map
--- dist/models/Exceptions/UnexpectedException.d.ts
+++
@@ -1,5 +0,0 @@
-import BaseException from './BaseException';
-export default class UnexpectedException extends BaseException {
-    message: string;
-}
-//# sourceMappingURL=UnexpectedException.d.ts.map
